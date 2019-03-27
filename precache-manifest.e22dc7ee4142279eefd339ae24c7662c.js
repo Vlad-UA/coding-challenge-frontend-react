cdaad60bd26d65dc7a97d701896500ea725926cf
Vladyslav Kulyk <vladyslav.kulyk@gmail.com>
2019-03-27 19:30:54 +0200

    Updates

---
+++ precache-manifest.e22dc7ee4142279eefd339ae24c7662c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "37738632e06382244b00d63364a39e94",
+    "url": "/coding-challenge-frontend-react/static/media/logo.37738632.png"
+  },
+  {
+    "revision": "64f13d3b42af5660cb23",
+    "url": "/coding-challenge-frontend-react/static/js/runtime~main.ab36a81e.js"
+  },
+  {
+    "revision": "f2231f5c5b20e89e3432",
+    "url": "/coding-challenge-frontend-react/static/js/main.875e62be.chunk.js"
+  },
+  {
+    "revision": "e5aee3c3a4e6bf531538",
+    "url": "/coding-challenge-frontend-react/static/js/2.de84e079.chunk.js"
+  },
+  {
+    "revision": "f2231f5c5b20e89e3432",
+    "url": "/coding-challenge-frontend-react/static/css/main.4e6a418c.chunk.css"
+  },
+  {
+    "revision": "ed3379766a87d6047ac21f4b77de0853",
+    "url": "/coding-challenge-frontend-react/index.html"
+  }
+];
